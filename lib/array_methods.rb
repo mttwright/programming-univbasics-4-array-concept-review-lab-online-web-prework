9f5ff540f68dc0740c84956a3b473fb0bd92e7a5
virtual-frame-7789 <mttwright@gmail.com>
2019-08-31 17:34:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -43,5 +43,19 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  
+  counter = 0
+  min_value = 0
+
+  while array[counter] do
+    
+    if array[counter] < min_value
+      min_value = array[counter]
+    end
+  
+  counter += 1
+  end
+  
+min_value
+  
 end
