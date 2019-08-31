7a0279d0a394e47714f5f8f392e0ce7be145fefa
virtual-frame-7789 <mttwright@gmail.com>
2019-08-31 17:37:33 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,61 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  counter = 0
+  
+  while array[counter] do
+    
+    if array[counter] == value_to_find
+      
+      return counter
+      
+    else
+      counter += 1
+      
+    end
+    
+  end
+  
+  nil
+  
+  
 end
 
+
+
 def find_max_value(array)
-  # Add your solution here
+  
+  counter = 0
+  max_value = 0
+  
+  while array[counter] do
+    
+    if array[counter] > max_value
+      max_value = array[counter]
+    end
+    
+    counter += 1
+    
+  end
+  
+  max_value
+  
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  
+  counter = 0
+  min_value = array[counter]
+
+  while array[counter] do
+    
+    if array[counter] < min_value
+      min_value = array[counter]
+    end
+  
+  counter += 1
+  end
+  
+min_value
+  
 end
